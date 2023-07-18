ec9554b88b4d68eb8281f5750bb492fae5c5a736
samuel <samuelkariuki1@student.moringaschool.com>
2023-07-18 23:50:18 +0300

    jsx lab done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name, city } from "../data/data";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const h1Style = {
+    color: "firebrick"
+  };
+
+  return (
+    <div id="home">
+      <h1 style={h1Style}>
+        {`${name} is a Web Developer from ${city}`}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
